a20f87d9c45f2773db5249ef2e3790aaeab9ed26
matiaz.orellana <matiaz.orellana@icloud.com>
2022-08-28 21:20:51 -0300

    220827

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
 const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
@@ -16,6 +17,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(logger('common', {
+    stream: fs.createWriteStream('./logs/access.log', {flags: 'a'})
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
