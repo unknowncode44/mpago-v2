7b5806364f42144d9607a66b30ce57667d3b15e4
matiaz.orellana <matiaz.orellana@icloud.com>
2022-08-30 00:06:45 -0300

    220830

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -221,9 +221,9 @@ nextBtnFirst.addEventListener("click", async () => {
                                         if(partner.socio == club_id.value) {
                                             partnerName = 'SI'
                                             rPrice.value = originalPrice
-                                            discount = Number(rPrice.value)*0.2
+                                            discount = Number(rPrice.value)*0.1
                                             originalPrice = `${(rPrice.value).toString()}`;
-                                            rPrice.value = (Number(rPrice.value)*0.8).toString();   
+                                            rPrice.value = (Number(rPrice.value)*0.9).toString();   
                                         }
                                     })
 
